b991a0c63d636ca9c98389208365c672c7b4343e
Hector <hector@peopleofthe.cloud>
2024-12-04 17:23:09 +0100

    First commit

---
+++ Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gametopia.SteamIntegration.Controllers
+{
+    [ApiController]
+    [Route("api/steam")]
+    public class GameController : ControllerBase
+    {
+        private readonly GameService _gameService;
+
+        public GameController(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet("user-games/{steamId}")]
+        public async Task<IActionResult> GetUserGames(string steamId)
+        {
+            var games = await _gameService.GetUserGamesAsync(steamId);
+            return Ok(games);
+        }
+    }
+}
---
+++ Services/GameService.cs
@@ -0,0 +1,31 @@
+﻿using Gametopia.Contracts.SteamIntegration;
+using Gametopia.Contracts.SteamIntegration.SteamGamesService;
+using System.Text.Json;
+
+public class GameService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+    private string _apiKey;
+
+    public GameService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+
+        _apiKey = _configuration["Steam:ApiKey"];
+    }
+
+    public async Task<List<GameDto>> GetUserGamesAsync(string steamId)
+    {
+        var client = _httpClientFactory.CreateClient("SteamAPI");
+        var response = await client.GetAsync($"IPlayerService/GetOwnedGames/v0001/?key={_apiKey}&steamid={steamId}&format=json");
+
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var data = JsonSerializer.Deserialize<SteamGamesResponse>(jsonResponse);
+
+        return data?.Games ?? new List<GameDto>();
+    }
+}
